376eaf4b5dfc45ec61c81791204b3bb4559fa263
Tushar00018 <satalingoltushar@gmail.com>
2024-07-11 22:14:06 +0530

    first commit

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,5 +1,5 @@
-const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary");
+const CloudinaryStorage = require("multer-storage-cloudinary");
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
